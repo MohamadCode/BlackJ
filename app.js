3de958294f0dd07eb63b59c7f036698191b6be7f
MohamadCode <m-munla2004@hotmail.com>
2023-05-09 23:59:07 +0300

    all conditions

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-let firstCard = 4
+let firstCard = 7
 let secondCard = 14
 let sum = firstCard + secondCard
 
 
 if (sum < 21) {
     console.log("Do you want to draw a new card? 🙂")
+} 
+else if (sum === 21) {
+    console.log("Hooray! You've got Blackjack! 🥳")
+}
+else if (sum > 21) {
+    console.log("GAME OVER! 😭")
 }
