ae570e5f7bc7c44f8090958769958a7c1a5c7563
MohamadCode <m-munla2004@hotmail.com>
2023-05-11 01:25:03 +0300

    message

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ let secondCard = 14
 let sum = firstCard + secondCard
 let hasblackJack = false
 let isAlive = true
-
+let message = ""
 
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } 
 else if (sum === 21) {
-    console.log("Hooray! You've got Blackjack! 🥳")
+    message = "Hooray! You've got Blackjack! 🥳"
     hasblackJack = true
 }
 else {
-    console.log("GAME OVER! 😭")
+    message = "GAME OVER! 😭"
     isAlive = false
 }
 
 console.log(isAlive)
 console.log(hasblackJack)
+console.log(message)
