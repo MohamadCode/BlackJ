594f30b8abc4309f31fb573b8e5e912d2ef01564
MohamadCode <m-munla2004@hotmail.com>
2023-05-10 15:26:02 +0300

    true-false

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-let firstCard = 9g
+let firstCard = 7
 let secondCard = 14
 let sum = firstCard + secondCard
+let hasblackJack = false
 
-
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } 
 else if (sum === 21) {
     console.log("Hooray! You've got Blackjack! 🥳")
+    hasblackJack = true
 }
 else {
     console.log("GAME OVER! 😭")
 }
+
+
+console.log(hasblackJack)
