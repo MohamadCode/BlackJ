fea15f4ae2428ac83bc654fb1c795b034cac6017
MohamadCode <m-munla2004@hotmail.com>
2023-05-16 17:42:53 +0300

    random-card

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-let firstCard = gerRandomCard()
-let secondCard = gerRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
 let hasblackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -27,6 +26,12 @@ let randomNumer = Math.floor( Math.random()*13 ) + 1
 
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
     renderGame()
 
 
